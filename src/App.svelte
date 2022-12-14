10396eb85b88563f892655e5a9ac3d803edbe01d
Mona Lisa <joorysalam786@gmail.com>
2022-12-14 12:08:21 +0400

    quick fixes

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,7 @@
     console.log({ output });
     return output % 1 !== 0 ? parseFloat(output + "").toPrecision(4) : output;
   }
+
   function addToEquation(value) {
     if (numberInput === "") {
       if (["+", "-", "/", "*", "%"].includes(value)) {
@@ -25,11 +26,23 @@
     if (numberInput !== "") output = result().toString();
     numberInput = "";
   }
+
+  function del() {
+    numberInput = numberInput.substring(0, numberInput.length - 1);
+    let outputStr = output + "";
+    outputStr = outputStr.substring(0, outputStr.length - 1);
+    if (outputStr[outputStr.length - 1] === ".")
+      outputStr = outputStr.substring(0, outputStr.length - 1);
+    output = +outputStr;
+  }
 </script>
 
 <section class="calculator">
   <div class="display">
-    <div class="input">{numberInput || formatOutput(output)}</div>
+    <div class="input">
+      {numberInput.replaceAll("*", "x").replaceAll("/", "÷") ||
+        formatOutput(output)}
+    </div>
   </div>
 
   <div class="buttons">
@@ -42,7 +55,7 @@
     >
       AC
     </button>
-    <button class="b-extra"> +/- </button>
+    <button class="b-delete" on:click={() => del()}> DEL </button>
     <button class="b-percentage" on:click={() => addToEquation("%")}>
       %
     </button>
@@ -71,7 +84,10 @@
 
 <style>
   .calculator {
-    background-color: rgb(227, 227, 226);
+    --textcolor: rgb(54 48 83);
+    --bgcolor: rgb(227, 227, 226);
+
+    background-color: var(--bgcolor);
     border-radius: 24px;
     padding: 16px;
     width: max-content;
@@ -79,7 +95,7 @@
 
   .display {
     height: 200px;
-    background-color: rgb(54 48 83);
+    background-color: var(--textcolor);
     margin: 4px;
     border-radius: 16px;
     display: flex;
@@ -98,10 +114,10 @@
   button {
     height: 70px;
     width: 70px;
-    background-color: rgb(227, 227, 226);
+    background-color: var(--bgcolor);
     font-family: "Jetbrains Mono", monospace;
     font-size: 30px;
-    color: rgb(51, 52, 80);
+    color: var(--textcolor);
     display: grid;
     border-radius: 100%;
     text-align: center;
@@ -114,12 +130,8 @@
     background-color: #f9f9ff;
   }
 
-  .b-clear {
-    background-color: #f9f9ff;
-  }
-
   .b-equals:hover {
-    background-color: rgb(54 48 83);
+    background-color: var(--textcolor);
     box-shadow: 0px 7px 8px -4px rgb(0 0 0 / 20%),
       0px 12px 17px 2px rgb(0 0 0 / 14%), 0px 5px 22px 4px rgb(0 0 0 / 12%);
   }
@@ -129,10 +141,17 @@
     grid-row: 5;
     border-radius: 50px;
     width: auto;
-    background-color: rgb(54 48 83);
+    background-color: var(--textcolor);
     color: rgb(221, 220, 220);
     box-shadow: 0px 3px 5px -1px rgb(0 0 0 / 20%),
       0px 6px 10px 0px rgb(0 0 0 / 14%), 0px 1px 18px 0px rgb(0 0 0 / 12%);
     transition: all 250ms ease-in;
+  }
+
+  .b-delete {
+    font-size: 28px;
+  }
+
+  @media screen and (max-width: 375) {
   }
 </style>
